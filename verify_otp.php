4836e7205332d21074ca48dea44965cbffdfff3d
dsoyavatmal <shreetech15@gmail.com>
2024-07-26 08:39:21 +0530

    uid

--- verify_otp.php
+++ verify_otp.php
@@ -29,9 +29,9 @@ if (isset($_SESSION['uid'])) {
         <div class="col-md-3"></div>
         <div class="col-md-5 mt-5">
           <div class="bg-white p-5 rounded-3 shadow-sm border">
-          
-          <form method="post" action="verify_send_otp.php">
           <h3><span class="badge badge-primary mt-2 p-2 "><?php  echo "UID: " . htmlspecialchars($uid); ?></span></h3> 
+          <form method="post" action="verify_send_otp.php">
+      
               <p class="text-center text-success" style="font-size: 5.5rem;"><i class="fa-solid fa-envelope-circle-check"></i></p>
               <p class="text-center text-center h5">Please check your email</p>
               <p class="text-muted text-center">We've sent a code on your registered mobile number <label id="lastFourDigits"></label></p>
