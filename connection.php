5e0979c8b1b104f793246f8858246e4877feef3d
dsoyavatmal <shreetech15@gmail.com>
2024-07-31 07:35:12 +0530

    gfgfdg

--- connection.php
+++ connection.php
@@ -12,7 +12,7 @@
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+echo $conn;
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
